ec88268ffaeac0ff3e209071ca7229b7d74f5cac
Mageshmage26 <100910554+Mageshmage26@users.noreply.github.com>
2026-07-24 17:25:54 +0530

    Committing 1 item from workspace 57d84005-cae9-4623-8753-45f36fb2c4cb

--- demo/BusinessReadyData.Warehouse/dbo/Tables/Orders.sql
+++ demo/BusinessReadyData.Warehouse/dbo/Tables/Orders.sql
@@ -2,6 +2,6 @@ CREATE TABLE [dbo].[Orders] (
 
 	[OrderID] int NULL, 
 	[CustomerID] int NULL, 
-	[CreditCard] varchar(20) NULL,
-	[SaleAmount] DECIMAL(10,2) NULL
+	[CreditCard] varchar(20) NULL, 
+	[SaleAmount] decimal(10,2) NULL
 );
